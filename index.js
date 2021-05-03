ce06bf9b0fdb4e1470348ca2800ed262b0bf2c1a
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-03 16:47:36 -0700

    add a promise

---
+++ index.js
@@ -0,0 +1,6 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => )
+    .then(json => {
+        const blogPost = document.createElement('div');
+        document.body.appendChild('div');
+    })
