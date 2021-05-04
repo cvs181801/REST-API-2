7932189fa749df66cdff8acac5d80894d35049ef
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-04 10:12:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-fetch('https://jsonplaceholder.typicode.com/posts') 
- //   method: 'POST',
- //   body: json.stringify({
-//        name: 'title',
- //   }),
-//})  
-    .then(response => response.json())
-    .then(json => {
-        json.forEach (post => {
-            const div = document.createElement('div');
-            div.innerHTML = title.json;
-            document.body.appendChild(div);
+// fetch('https://jsonplaceholder.typicode.com/posts') 
+//     .then(response => response.json())
+//     .then(json => {
+//         json.forEach (post => {
+//             const div = document.createElement('div');
+//             div.innerHTML = title.json;
+//             document.body.appendChild(div);
+//         }) 
+//     })
+        
+    fetch('https://jsonplaceholder.typicode.com/posts') 
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(post => {
+            const newDiv = document.createElement('div');
+            newDiv.innerHTML = post.title;
+            document.body.appendChild(newDiv);
         }) 
     })
-        
-        
     
