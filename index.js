8ca40f114faebc4ccc83e517d0cfcc3160ce625e
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-03 17:10:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ fetch('https://jsonplaceholder.typicode.com/posts/1', {
     body: json.stringify({
         name: 'title',
     }),
-})
-    .then(response => {
+})  
+    .then(response => response.json)
+    .then(json => {
         const blogPost = document.createElement('div');
         div.innerHTML = title.json;
         document.body.appendChild('div');
