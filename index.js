d1b6ee38f01d26aea3afabc9f25184f41bebeb6e
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-05 11:23:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,9 @@
     fetch('https://jsonplaceholder.typicode.com/posts') 
     .then(res => res.json())
     .then(data => {
+        let n = 0;
         data.forEach(post => {
+            n++;
             for (let n=0; n < posts; n++) {
             const newDiv = document.createElement('div');
             newDiv.innerHTML = `${n}. ${post.title}`;
