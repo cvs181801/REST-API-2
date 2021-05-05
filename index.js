e7cc57ed0f3228254f57bd97c4daba8bf9ed690f
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-05 11:35:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -24,14 +24,26 @@
     //     }) 
     // })
     
-    const list = document.querySelector("ol");
+    // const list = document.querySelector("ol");
 
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => {
-        data.forEach(item => {
-          const li = document.createElement("li");
-          li.innerText = item.title;
-          list.appendChild(li);
-        });
-      });
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     data.forEach(item => {
+    //       const li = document.createElement("li");
+    //       li.innerText = item.title;
+    //       list.appendChild(li);
+    //     });
+    //   });
+
+      const div = document.querySelector('div')
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((item, index) => {
+      const paragraph = document.createElement("p");
+      paragraph.innerText = `${index}. ${item.title}`;
+      div.appendChild(paragraph)
+    });
+  });
