e626b2031ba04b273776dda20fe3fecc6924a6b7
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-05 11:25:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,9 @@
         let n = 0;
         data.forEach(post => {
             n++;
-            for (let n=0; n < posts; n++) {
             const newDiv = document.createElement('div');
             newDiv.innerHTML = `${n}. ${post.title}`;
             document.body.appendChild(newDiv);
-            }
         }) 
     })
     
